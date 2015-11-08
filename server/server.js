0324c663bb36da1bc77b2d2ef6b0e9c97f195b62
Fangting <fangtingprahl@gmail.com>
2015-11-08 13:33:18 -0600

    server - setup route handler for trending page

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-
+var Quizbot = require('./quizbotModel.js');
 var app = express();
 
 // static serve
@@ -10,6 +10,18 @@ app.use(express.static(__dirname + './../www'));
 // dev request logger
 app.use(morgan('dev'));
 
+app.get('/quizbot', function(req, res){
+    Quizbot.find({}, function(err, quizbot){
+      if (err) { throw err; }
+      console.log('server.js quizbot', quizbot);
+      return quizbot;
+    })
+    .then(function(quizbot){
+      res.send(quizbot);
+    })
+})
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/quizbot');
 // local server
 var port = process.env.PORT || 8000;
 app.listen(port, function(){
