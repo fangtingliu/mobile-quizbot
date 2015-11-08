2156993f22a61b4cae96fdd094dec684380ccdd2
Fangting <fangtingprahl@gmail.com>
2015-11-08 13:32:46 -0600

    server - create quizzes schema

---
+++ server/quizzesModel.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Quizbot = require('quizbotModel.js');
+
+var Quizzes = new Schema({
+  userEmail: {
+    type: String,
+    required: true
+  },
+
+  selection: {
+    type: Array,
+    required: true
+  },
+
+  quiz: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Quizbot'
+  }]
+})
+
+module.exports = mongoose.model('Quizzes', Quizzes);
